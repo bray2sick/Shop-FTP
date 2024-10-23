0166fd997ec13d6c01751c0c4607eb5fb531a9cd
Braydon <braydonpettit24@outlook.com>
2024-10-23 23:46:20 +1100

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,74 @@
+// Function to get URL parameters
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    const results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+// Function to update product details
+function updateProductDetails() {
+    const productName = getUrlParameter('name');
+    const productPrice = getUrlParameter('price');
+
+    const productNameElement = document.getElementById('productName');
+    const productPriceElement = document.getElementById('productPrice');
+
+    if (productName && productNameElement) {
+        productNameElement.textContent = productName;
+    }
+    if (productPrice && productPriceElement) {
+        productPriceElement.textContent = `$${productPrice}`;
+    }
+}
+
+// Call the function to update product details on page load
+document.addEventListener('DOMContentLoaded', updateProductDetails);
+
+// Global variable to keep track of the current image index
+let currentImageIndex = 0;
+const images = ['front', 'back'];
+
+// Function to show image
+function showImage(imageType) {
+    const fullImage = document.getElementById('fullImage');
+    const fullImageLink = document.getElementById('fullImageLink');
+    if (imageType === 'front') {
+        fullImage.src = '../../../images/front.png';
+        fullImageLink.href = '../../../images/front.png';
+        currentImageIndex = 0;
+    } else if (imageType === 'back') {
+        fullImage.src = '../../../images/back.png';
+        fullImageLink.href = '../../../images/back.png';
+        currentImageIndex = 1;
+    }
+}
+
+// Initialize Magnific Popup
+$(document).ready(function() {
+    $('.full-image a').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+        closeBtnInside: false,
+        closeOnContentClick: true,
+        mainClass: 'mfp-fade'
+    });
+});
+
+// FlexSlider 
+$(document).ready(function () {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        directionNav: false,
+        controlNav: false,
+        slideshow: true,
+        slideshowSpeed: 3000, // 3 seconds
+    });
+});
+
+// Disable right-click
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
